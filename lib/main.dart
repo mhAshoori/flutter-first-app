5ba2a2ee2c61e92d5b7e6bd371f71d445bc6deff
Mohammad Hossein Ashoori <mh_ashoori@yahoo.com>
2021-08-25 10:30:31 +0430

    Add some Style to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text("My First App"),
-      centerTitle: true,
-      backgroundColor: Colors.amberAccent,
-    ),
-    body: Center(
-      child: Text('Hello Mammad'),
-    ),
-    floatingActionButton: FloatingActionButton(onPressed: null,
-      child: Text('Click'),
-    ),
-  ),
-));
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "My First App",
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.deepOrange[300],
+        ),
+        body: Center(
+          child: Text(
+            'My Name is BluBeast!',
+            style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1.5,
+                color: Colors.grey[500],
+                fontFamily: 'IndieFlower'
+            ),
 
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => {},
+          backgroundColor: Colors.indigo,
+          child: Text('Click'),
+        ),
+      ),
+    ));
