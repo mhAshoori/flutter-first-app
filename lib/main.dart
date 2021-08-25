d5fbbae3a3d1a24fb0dabfc4198d58c92865e018
Mohammad Hossein Ashoori <mh_ashoori@yahoo.com>
2021-08-25 14:39:00 +0430

    Add app content to a Stateless class "Home"

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,40 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "My First App",
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.deepOrange[300],
-        ),
-        body: Center(
-          child: Text(
-            'My Name is RedArmy!',
-            style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 1.5,
-                color: Colors.blue[500],
-                fontFamily: 'IndieFlower'
-            ),
+      home: Home()
+    ));
 
-          ),
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "My First App",
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => {},
-          backgroundColor: Colors.indigo,
-          child: Text('Blink'),
+        centerTitle: true,
+        backgroundColor: Colors.deepOrange[300],
+      ),
+      body: Center(
+        child: Text(
+          'My Name is Mammad!',
+          style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1.5,
+              color: Colors.blue[500],
+              fontFamily: 'IndieFlower'
+          ),
+
         ),
       ),
-    ));
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        backgroundColor: Colors.indigo,
+        child: Text('Blink'),
+      ),
+    );
+  }
+}
